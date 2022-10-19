720c1da8725e985a14168a1f444ee791598c5376
Delcione Lopes da Silva <delcionelopes@gmail.com>
2022-10-19 17:13:14 -0300

    home

--- resources/views/layouts/page.blade.php
+++ resources/views/layouts/page.blade.php
@@ -38,7 +38,7 @@
                       <span class="caret"></span>
                 @endauth
 
-                <a class="navbar-brand" href="{{route('page.master')}}">Bem-vindo ao PRODAP!</a>
+                <a class="navbar-brand" href="https://www.prodap.ap.gov.br">PRODAP</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                     Menu
                     <i class="fas fa-bars"></i>
